3f290bbfae333455ce91584ab197b3671104569a
Marschant <marschant@gmail.com>
2018-08-27 12:15:47 +0200

    Login Form made using reactive components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from './material.module';
-
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { MaterialModule } from './material.module';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { NgModule } from '@angular/core';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +31,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
